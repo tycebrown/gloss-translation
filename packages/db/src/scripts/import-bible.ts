e3242c0ad8bb80264366048123f91d7a52c77911
tycebrown <91034018+tycebrown@users.noreply.github.com>
2023-12-05 22:03:53 -0600

    Too much for one commit :(
    data added, seed.dump cleared (to be filled with new import-bible data and stuff), basic usfm data adding.
    
    dealing with issues with alternative chapters and verses (which the usfm parser doesn't use by default, but sheldon's data does) and ktiv and qere readings (which the usfm someimtes goes with one or the other, indicated by footnote)

--- packages/db/src/scripts/import-bible.ts
+++ packages/db/src/scripts/import-bible.ts
@@ -1,12 +1,22 @@
+import { readFileSync } from 'fs';
 import { bookKeys } from '../../../../data/book-keys';
 import { morphologyData } from '../../../../data/morphology';
 import { PrismaClient } from '@prisma/client';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const usfm = require('usfm-js');
+
 const client = new PrismaClient();
 
 const STRONGS_REGEX = / \[e\]/;
-
+// incoporate unfolding word. strong's syntax: {prefixes}:{base_id}{letter}
+// look: applies only to bible, only to lex, or both?
+// add, also see: need data given of bible? or unfolding word?
+// vas and cas
 async function run() {
+  await client.word.deleteMany();
+  await client.verse.deleteMany();
+  await client.book.deleteMany();
   const wordData: {
     id: string;
     text: string;
@@ -32,13 +42,55 @@ async function run() {
       },
     });
 
+    const rawUsfmData = usfm.toJSON(
+      readFileSync(
+        `data/unfoldingWord/${(bookIndex + 1)
+          .toString()
+          .padStart(2, '0')}-${bookKey.toUpperCase()}.usfm`,
+        'utf-8'
+      )
+    );
+
+    const usfmChapters = rawUsfmData.chapters;
+
     for (let chapterIndex = 0; chapterIndex < chapters.length; chapterIndex++) {
       const verses = chapters[chapterIndex];
       const chapterNumber = chapterIndex + 1;
+      const usfmVerses = usfmChapters[chapterNumber];
 
       for (let verseIndex = 0; verseIndex < verses.length; verseIndex++) {
-        const words = verses[verseIndex];
+        let words = verses[verseIndex].filter(
+          ([text, , english, grammar]) =>
+            !(english === ' - ' && grammar === undefined) && !(text === 'ס')
+        );
+        words = words
+          .filter(([text], index) => {
+            if (text.startsWith('[') && text.endsWith(']')) {
+              if (
+                words[index - 1] &&
+                words[index - 1][0].startsWith('(') &&
+                words[index - 1][0].endsWith(')')
+              ) {
+                return true;
+              } else if (
+                words[index + 1] &&
+                words[index + 1][0].startsWith('(') &&
+                words[index + 1][0].endsWith(')')
+              ) {
+                return true;
+              }
+              return false;
+            }
+            return true;
+          })
+          .filter(([text]) => {
+            return !(text.startsWith('(') && text.endsWith(')'));
+          });
+
         const verseNumber = verseIndex + 1;
+        const usfmWords = usfmVerses[verseNumber]['verseObjects'].filter(
+          (word: { type: string }) => word.type === 'word'
+        );
 
         const verseId = [
           book.id.toString().padStart(2, '0'),
@@ -56,14 +108,33 @@ async function run() {
         });
 
         for (let wordIndex = 0; wordIndex < words.length; wordIndex++) {
-          const [text, , english, grammar = '', rawStrongs = '????'] =
-            words[wordIndex];
+          const [, , english, grammar = '', rawStrongs = ''] = words[wordIndex];
 
-          // We clean the strongs codes since the hebrew ones have some extra characters.
-          // We also prefix the code with a language code based on the book.
-          const strongs = `${book.id < 40 ? 'H' : 'G'}${rawStrongs
-            .replace(STRONGS_REGEX, '')
-            .padStart(4, '0')}`;
+          console.log(usfmWords[wordIndex]);
+          console.log(words[wordIndex]);
+
+          const { text, strong: usfmStrongsWithForm } = usfmWords[wordIndex];
+          const usfmStrongs = stripForm(usfmStrongsWithForm);
+
+          const sheldonStrongs =
+            rawStrongs !== ''
+              ? `${book.id < 40 ? 'H' : 'G'}${rawStrongs
+                  .replace(STRONGS_REGEX, '')
+                  .padStart(4, '0')}`
+              : '';
+
+          const strongs = usfmStrongs || sheldonStrongs;
+
+          if (eStrongToSimple(usfmStrongs) !== sheldonStrongs) {
+            console.log(
+              `Warning; usfmStrongs does not equal sheldonStrongs: '${eStrongToSimple(
+                usfmStrongs
+              )}' !== '${sheldonStrongs}'`
+            );
+            console.log(
+              `  ----- in book: ${bookKey}, chapter: ${chapterNumber}, verse: ${verseNumber}, word: ${wordIndex}`
+            );
+          }
 
           // We have to accumulate word data until we have inserted all of the lemma data.
           const wordId = `${verseId}${(wordIndex + 1)
@@ -142,4 +213,12 @@ async function run() {
   await client.$disconnect();
 }
 
+function stripForm(strongsWithForm: string) {
+  return strongsWithForm.slice(
+    strongsWithForm.lastIndexOf(':') + 1 || undefined
+  );
+}
+function eStrongToSimple(eStrong: string) {
+  return eStrong.replace(/[a-z]/, '');
+}
 run();
