45e66bfd665c0bd4a7c9a864764d2f57bbeaa00e
tycebrown <91034018+tycebrown@users.noreply.github.com>
2023-12-09 13:05:30 -0600

    Converted usfm data to array (rough); doesn't quite work

--- packages/db/src/scripts/import-bible.ts
+++ packages/db/src/scripts/import-bible.ts
@@ -2,6 +2,7 @@ import { readFileSync } from 'fs';
 import { bookKeys } from '../../../../data/book-keys';
 import { morphologyData } from '../../../../data/morphology';
 import { PrismaClient } from '@prisma/client';
+import { assert } from 'console';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const usfm = require('usfm-js');
@@ -9,14 +10,13 @@ const usfm = require('usfm-js');
 const client = new PrismaClient();
 
 const STRONGS_REGEX = / \[e\]/;
-// incoporate unfolding word. strong's syntax: {prefixes}:{base_id}{letter}
-// look: applies only to bible, only to lex, or both?
-// add, also see: need data given of bible? or unfolding word?
-// vas and cas
+// TODO: compress adjacent (with same storngs), make sure to use sheldon hebrew text.
 async function run() {
+  await client.gloss.deleteMany();
   await client.word.deleteMany();
   await client.verse.deleteMany();
   await client.book.deleteMany();
+  await client.language.deleteMany();
   const wordData: {
     id: string;
     text: string;
@@ -31,7 +31,7 @@ async function run() {
     };
   } = {};
 
-  for (let bookIndex = 0; bookIndex < bookKeys.length; bookIndex++) {
+  for (let bookIndex = 0; bookIndex < (39 || bookKeys.length); bookIndex++) {
     const bookKey = bookKeys[bookIndex];
     const chapters = morphologyData[bookKey];
 
@@ -42,21 +42,44 @@ async function run() {
       },
     });
 
-    const rawUsfmData = usfm.toJSON(
-      readFileSync(
-        `data/unfoldingWord/${(bookIndex + 1)
-          .toString()
-          .padStart(2, '0')}-${bookKey.toUpperCase()}.usfm`,
-        'utf-8'
+    const sheldonData = morphologyData[bookKey].map((value) =>
+      value.map((value) =>
+        value
+          .filter(
+            ([text, , english, grammar]) =>
+              !(english === ' - ' && grammar === undefined) && !(text === 'ס')
+          )
+          .filter(([text], index, words) => {
+            if (text.startsWith('[') && text.endsWith(']')) {
+              if (
+                words[index - 1] &&
+                words[index - 1][0].startsWith('(') &&
+                words[index - 1][0].endsWith(')')
+              ) {
+                return true;
+              } else if (
+                words[index + 1] &&
+                words[index + 1][0].startsWith('(') &&
+                words[index + 1][0].endsWith(')')
+              ) {
+                return true;
+              }
+              return false;
+            }
+            return true;
+          })
+          .filter(([text]) => {
+            return !(text.startsWith('(') && text.endsWith(')'));
+          })
       )
     );
 
-    const usfmChapters = rawUsfmData.chapters;
+    buildUsfmBookData({ book, sheldonData });
+    continue;
 
     for (let chapterIndex = 0; chapterIndex < chapters.length; chapterIndex++) {
       const verses = chapters[chapterIndex];
       const chapterNumber = chapterIndex + 1;
-      const usfmVerses = usfmChapters[chapterNumber];
 
       for (let verseIndex = 0; verseIndex < verses.length; verseIndex++) {
         let words = verses[verseIndex].filter(
@@ -88,9 +111,6 @@ async function run() {
           });
 
         const verseNumber = verseIndex + 1;
-        const usfmWords = usfmVerses[verseNumber]['verseObjects'].filter(
-          (word: { type: string }) => word.type === 'word'
-        );
 
         const verseId = [
           book.id.toString().padStart(2, '0'),
@@ -107,14 +127,15 @@ async function run() {
           },
         });
 
-        for (let wordIndex = 0; wordIndex < words.length; wordIndex++) {
-          const [, , english, grammar = '', rawStrongs = ''] = words[wordIndex];
+        for (
+          let sheldonWordIndex = 0;
+          sheldonWordIndex < words.length;
+          sheldonWordIndex++
+        ) {
+          const [text, , english, grammar = '', rawStrongs = ''] =
+            words[sheldonWordIndex];
 
-          console.log(usfmWords[wordIndex]);
-          console.log(words[wordIndex]);
-
-          const { text, strong: usfmStrongsWithForm } = usfmWords[wordIndex];
-          const usfmStrongs = stripForm(usfmStrongsWithForm);
+          console.log(words[sheldonWordIndex]);
 
           const sheldonStrongs =
             rawStrongs !== ''
@@ -123,21 +144,10 @@ async function run() {
                   .padStart(4, '0')}`
               : '';
 
-          const strongs = usfmStrongs || sheldonStrongs;
-
-          if (eStrongToSimple(usfmStrongs) !== sheldonStrongs) {
-            console.log(
-              `Warning; usfmStrongs does not equal sheldonStrongs: '${eStrongToSimple(
-                usfmStrongs
-              )}' !== '${sheldonStrongs}'`
-            );
-            console.log(
-              `  ----- in book: ${bookKey}, chapter: ${chapterNumber}, verse: ${verseNumber}, word: ${wordIndex}`
-            );
-          }
+          const strongs = sheldonStrongs;
 
           // We have to accumulate word data until we have inserted all of the lemma data.
-          const wordId = `${verseId}${(wordIndex + 1)
+          const wordId = `${verseId}${(sheldonWordIndex + 1)
             .toString()
             .padStart(2, '0')}`;
           wordData.push({
@@ -213,12 +223,131 @@ async function run() {
   await client.$disconnect();
 }
 
+async function buildUsfmBookData({
+  book,
+  sheldonData,
+}: {
+  book: any;
+  sheldonData: any;
+}) {
+  // build database.
+  const rawData = usfm.toJSON(
+    readFileSync(
+      `data/unfoldingWord/${book.id
+        .toString()
+        .padStart(2, '0')}-${book.name.toUpperCase()}.usfm`,
+      'utf-8'
+    )
+  );
+
+  const runningAdjustedChapters: any = {};
+  let currentChapterNumber = 1;
+  let currentVerseNumber = 1;
+
+  for (const [chapterNumber, chapter] of Object.entries(rawData['chapters'])) {
+    currentChapterNumber = +chapterNumber.trim();
+    runningAdjustedChapters[currentChapterNumber] =
+      runningAdjustedChapters[currentChapterNumber] || {};
+
+    delete (chapter as any)['front'];
+    for (const [verseNumber, verse] of Object.entries(chapter as any)) {
+      currentVerseNumber = +verseNumber.trim();
+      for (const verseObject of (verse as any)['verseObjects']) {
+        runningAdjustedChapters[currentChapterNumber] =
+          runningAdjustedChapters[currentChapterNumber] || {};
+        const currentVerse = (runningAdjustedChapters[currentChapterNumber][
+          currentVerseNumber
+        ] =
+          runningAdjustedChapters[currentChapterNumber][currentVerseNumber] ||
+          []);
+        switch (verseObject['tag']) {
+          case 'w':
+            currentVerse.push({
+              text: verseObject['text'],
+              strong: verseObject['strong'],
+            });
+            break;
+          case 'ca':
+            currentChapterNumber = +verseObject['content'].trim();
+            break;
+          case 'va': {
+            const theVerseNumber = verseObject['content'].trim();
+            if (theVerseNumber.indexOf(':') === -1) {
+              currentVerseNumber = +theVerseNumber;
+            } else {
+              const [alternateChapterNumber, alternateVerseNumber] =
+                theVerseNumber.split(':');
+              currentChapterNumber = alternateChapterNumber;
+              currentVerseNumber = alternateVerseNumber;
+            }
+            break;
+          }
+          default:
+        }
+      }
+    }
+  }
+  console.log(book.name);
+
+  console.log(
+    compareShape(flattenChapters(runningAdjustedChapters), sheldonData)
+  );
+  console.log(
+    compareShape(sheldonData, flattenChapters(runningAdjustedChapters))
+  );
+}
+
+function flattenChapters(chapters: any) {
+  const resultingChapters: any[] = [];
+  Object.entries(chapters).forEach(
+    ([chapterNumber, chapterData]: [any, any]) => {
+      const currentChapter: any[] = (resultingChapters[+chapterNumber - 1] =
+        []);
+      Object.entries(chapterData).forEach(
+        ([verseNumber, verseData]: [any, any]) => {
+          currentChapter[+verseNumber - 1] = verseData;
+        }
+      );
+    }
+  );
+  return resultingChapters;
+}
+
+function compareShape(a: any, b: any, i?: number) {
+  if (Array.isArray(a) && Array.isArray(b)) {
+    console.log(`  ${(i ?? -1) + 1 ? `${i} ` : ''}${a.length} : ${b.length}`);
+    if (a.length !== b.length) return false;
+    for (let i = 0; i < a.length; ++i) {
+      if (!compareShape(a[i], b[i], i)) {
+        console.log(a[i]);
+        console.log(b[i]);
+        throw NaN;
+        return false;
+      }
+    }
+    return true;
+  }
+
+  return !(
+    (Array.isArray(a) && (Array.isArray(a[0]) || typeof b !== 'object')) ||
+    (Array.isArray(b) && (Array.isArray(b[0]) || typeof a !== 'object'))
+  );
+}
+
 function stripForm(strongsWithForm: string) {
   return strongsWithForm.slice(
     strongsWithForm.lastIndexOf(':') + 1 || undefined
   );
 }
+
 function eStrongToSimple(eStrong: string) {
   return eStrong.replace(/[a-z]/, '');
+}
+
+function suspend() {
+  return new Promise(() => {
+    const x = true;
+    while (x);
+  });
 }
 run();
