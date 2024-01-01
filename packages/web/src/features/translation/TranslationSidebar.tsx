e060ba5dea7df89f528b9a9afdb73c69b4dd9098
tycebrown <91034018+tycebrown@users.noreply.github.com>
2024-01-01 00:25:44 -0600

    refactor: clean, remove context

--- packages/web/src/features/translation/TranslationSidebar.tsx
+++ packages/web/src/features/translation/TranslationSidebar.tsx
@@ -6,17 +6,7 @@ import { Icon } from '../../shared/components/Icon';
 import LoadingSpinner from '../../shared/components/LoadingSpinner';
 import { parseVerseId } from './verse-utils';
 import DOMPurify from 'dompurify';
-import { ReactNode, createContext, useContext, useState } from 'react';
-
-type TranslationContextObject = {
-  language: string;
-  verse: Verse;
-  word: VerseWord;
-};
-
-const TranslationContext = createContext<TranslationContextObject | undefined>(
-  undefined
-);
+import { ReactNode, useState } from 'react';
 
 type TranslationSidebarProps = {
   language: string;
@@ -36,69 +26,75 @@ export const TranslationSidebar = ({
   const isHebrew = bookId < 40;
   const { t } = useTranslation(['common', 'translate']);
   return (
-    <TranslationContext.Provider value={{ language, verse, word }}>
-      <div
-        className="
+    <div
+      className="
         border-t h-[320px] flex flex-col gap-4 pt-3 flex-shrink-0
-        md:border-t-0 md:ltr:border-l md:rtl:border-r md:h-auto md:w-1/3 md:min-w-[360px] md:max-w-[480px] md:pt-0
+        md:border-t-0 md:ltr:border-l md:rtl:border-r md:h-auto md:w-1/3 md:min-w-[400px] md:max-w-[480px] md:pt-0
       "
-      >
-        <div className="ps-4">
-          <div className="flex flex-row items-center gap-4">
-            <button onClick={onClose} type="button">
-              <Icon icon="chevron-down" className="block md:hidden" />
-              <Icon icon="chevron-right" className="hidden md:block" />
-              <span className="sr-only">{t('common:close')}</span>
-            </button>
-            <span
-              className={
-                isHebrew ? 'font-hebrew text-2xl' : 'font-greek text-xl'
-              }
-            >
-              {word.text}
-            </span>
-            <span>{word.lemmaId}</span>
-          </div>
+    >
+      <div className="ps-4">
+        <div className="flex flex-row items-center gap-4">
+          <button onClick={onClose} type="button">
+            <Icon icon="chevron-down" className="block md:hidden" />
+            <Icon icon="chevron-right" className="hidden md:block" />
+            <span className="sr-only">{t('common:close')}</span>
+          </button>
+          <span
+            className={isHebrew ? 'font-hebrew text-2xl' : 'font-greek text-xl'}
+          >
+            {word.text}
+          </span>
+          <span>{word.lemmaId}</span>
         </div>
-        <SidePanelTabs />
       </div>
-    </TranslationContext.Provider>
+      <SidePanelTabs language={language} verse={verse} word={word} />
+    </div>
   );
 };
 
-const tabs = [
-  { title: 'BDB', buildContent: () => <BDBTab /> },
-  { title: 'Strongs', buildContent: () => <StrongsTab /> },
-  { title: 'Usage', buildContent: () => <UsageTab /> },
-  { title: 'Chapter', buildContent: () => <ChapterTab /> },
-  { title: 'Comments', buildContent: () => <CommentsTab /> },
+type TabProps = {
+  language: string;
+  verse: Verse;
+  word: VerseWord;
+};
+
+type TabData = {
+  title: string;
+  buildContent: (props: TabProps) => ReactNode;
+};
+
+const sidePanelTabs: TabData[] = [
+  { title: 'Lexicon', buildContent: (props) => <LexiconTab {...props} /> },
+  { title: 'Strongs', buildContent: (props) => <StrongsTab {...props} /> },
+  { title: 'Usage', buildContent: (props) => <UsageTab {...props} /> },
+  { title: 'Chapter', buildContent: (props) => <ChapterTab {...props} /> },
+  { title: 'Comments', buildContent: (props) => <CommentsTab {...props} /> },
 ];
 
-function SidePanelTabs() {
+function SidePanelTabs(props: TabProps) {
   const [activeTab, setActiveTab] = useState(0);
   return (
     <>
-      <div className="flex flex-row gap-x-0.5 xl:text-base md:text-sm border-b-2 ps-1">
-        {tabs.map(({ title }, i) => (
-          <div
+      <ol className="flex flex-row gap-x-0.5 xl:text-base md:text-sm border-b-2 ps-1">
+        {sidePanelTabs.map(({ title }, i) => (
+          <li
             className={`select-none px-2 py-1 border-t-2 rounded-t border-x-2 relative ${
               activeTab === i ? 'bg-white top-0.5' : 'bg-gray-300'
             }`}
             onClick={() => setActiveTab(i)}
           >
             {title}
-          </div>
+          </li>
         ))}
-      </div>
-      <div className="px-4 overflow-y-auto grow ">
-        {tabs[activeTab].buildContent()}
+      </ol>
+      <div className="px-4 overflow-y-auto grow">
+        {sidePanelTabs[activeTab].buildContent(props)}
       </div>
     </>
   );
 }
 
-function BDBTab() {
-  const { language, verse, word } = useContext(TranslationContext)!;
+function LexiconTab({ language, verse, word }: TabProps) {
   const lemmaResourcesQuery = useQuery(
     ['verse-lemma-resources', language, verse.id],
     () => apiClient.verses.findLemmaResources(verse.id)
@@ -134,67 +130,18 @@ function BDBTab() {
   );
 }
 
-function StrongsTab() {
-  const { language, verse, word } = useContext(TranslationContext)!;
-  return (
-    <>
-      <h1 className="mb-3 text-lg font-bold me-2">Strongs content</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex iure fugit
-        sequi dolore autem laboriosam adipisci consectetur vero eius quod?
-        Ratione veritatis quam esse perspiciatis maiores eveniet sint iusto
-        atque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
-        sapiente reprehenderit officiis tempore nostrum cupiditate, nemo quae
-        qui consectetur dignissimos rerum consequatur beatae natus ea totam
-        voluptatum autem hic dolores. Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Ex iure fugit sequi dolore autem laboriosam adipisci
-        consectetur vero eius quod? Ratione veritatis quam esse perspiciatis
-        maiores eveniet sint iusto atque? Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Quis sapiente reprehenderit officiis tempore nostrum
-        cupiditate, nemo quae qui consectetur dignissimos rerum consequatur
-        beatae natus ea totam voluptatum autem hic dolores. Lorem ipsum, dolor
-        sit amet consectetur adipisicing elit. Ex iure fugit sequi dolore autem
-        laboriosam adipisci consectetur vero eius quod? Ratione veritatis quam
-        esse perspiciatis maiores eveniet sint iusto atque? Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Quis sapiente reprehenderit
-        officiis tempore nostrum cupiditate, nemo quae qui consectetur
-        dignissimos rerum consequatur beatae natus ea totam voluptatum autem hic
-        dolores. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex
-        iure fugit sequi dolore autem laboriosam adipisci consectetur vero eius
-        quod? Ratione veritatis quam esse perspiciatis maiores eveniet sint
-        iusto atque? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Quis sapiente reprehenderit officiis tempore nostrum cupiditate, nemo
-        quae qui consectetur dignissimos rerum consequatur beatae natus ea totam
-        voluptatum autem hic dolores. Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Ex iure fugit sequi dolore autem laboriosam adipisci
-        consectetur vero eius quod? Ratione veritatis quam esse perspiciatis
-        maiores eveniet sint iusto atque? Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Quis sapiente reprehenderit officiis tempore nostrum
-        cupiditate, nemo quae qui consectetur dignissimos rerum consequatur
-        beatae natus ea totam voluptatum autem hic dolores. Lorem ipsum, dolor
-        sit amet consectetur adipisicing elit. Ex iure fugit sequi dolore autem
-        laboriosam adipisci consectetur vero eius quod? Ratione veritatis quam
-        esse perspiciatis maiores eveniet sint iusto atque? Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Quis sapiente reprehenderit
-        officiis tempore nostrum cupiditate, nemo quae qui consectetur
-        dignissimos rerum consequatur beatae natus ea totam voluptatum autem hic
-        dolores.
-      </p>
-    </>
-  );
+function StrongsTab({ language, verse, word }: TabProps) {
+  return <h1>Strongs content</h1>;
 }
 
-function UsageTab() {
-  const { language, verse, word } = useContext(TranslationContext)!;
+function UsageTab({ language, verse, word }: TabProps) {
   return <h1>Usage content</h1>;
 }
 
-function ChapterTab() {
-  const { language, verse, word } = useContext(TranslationContext)!;
+function ChapterTab({ language, verse, word }: TabProps) {
   return <h1>Chapter content</h1>;
 }
 
-function CommentsTab() {
-  const { language, verse, word } = useContext(TranslationContext)!;
+function CommentsTab({ language, verse, word }: TabProps) {
   return <h1>Comments content</h1>;
 }
